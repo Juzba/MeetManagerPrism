02b16868f0a41d83c842ad4855bdccd593b554e3
Jiří Strnadel <Juzba88@gmail.com>
2025-07-20 20:54:32 +0200

    Update manager events list

--- MeetManagerPrism/ViewModels/Manager/ManagerEventsViewModel.cs
+++ MeetManagerPrism/ViewModels/Manager/ManagerEventsViewModel.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using MeetManagerPrism.Data.Model;
+using MeetManagerPrism.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MeetManagerPrism.ViewModels.Manager
 {
-    public class ManagerEventsViewModel: BindableBase
+    public class ManagerEventsViewModel : BindableBase
     {
+        private readonly IDataService _dataService;
+
+        public ManagerEventsViewModel(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+
+        // EVENT LIST //
+        private ObservableCollection<Event> eventsList = [new Event(){ Name = "Muj Event", Description=" blabla"}];
+        public ObservableCollection<Event> EventsList
+        {
+            get { return eventsList; }
+            set { SetProperty(ref eventsList, value); }
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
